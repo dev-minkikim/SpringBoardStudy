e4495d06046f9894f6d063193df29a014c9ea9c4
dev-MinKi <alsrl8755@naver.com>
2020-05-31 17:56:43 +0900

    게시판 목록 기능 구현
    - boardMapper.xml에 select 코드 추가
    
    - DAO, DAOImpl에 boardMapper.xml에서 id가 select인 값을 가져와서 list형태로 반환
    
    - Service, ServiceImpl에서 dao에 있는 list()함수를 호출해서 반환
    
    - BoardController에서 DB -> DAO -> Service ->Controller로 가져온 데이터들을 jsp에 뿌려주는 작업 실시
       * model을 사용하여 데이터 넘김
    
    - list.jsp 작성하여 컨트롤러에서 넘겨받은 데이터를 화면에 나타나게 코드 작성

--- SpringBoard/src/main/java/kr/co/controller/BoardController.java
+++ SpringBoard/src/main/java/kr/co/controller/BoardController.java
@@ -1,9 +1,11 @@
 package kr.co.controller;
 
 import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,5 +34,16 @@ public class BoardController {
 		service.write(boardVO);
 		
 		return "redirect:/";
+	}
+	
+	//게시물 목록 조회
+	
+	@RequestMapping(value="/board/list",method=RequestMethod.GET)
+	public String list(Model model) throws Exception{
+		logger.info("list");
+		
+		model.addAttribute("list",service.list());
+		
+		return "board/list";
 	}
 }
